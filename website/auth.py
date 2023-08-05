5a9844a22161cdaffcbc09ec6ef6182b3d67ed96
Praise-Atadja <p.atadja@alustudent.com>
2023-08-05 14:39:52 +0100

    update navbar

--- website/auth.py
+++ website/auth.py
@@ -41,7 +41,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.home'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
